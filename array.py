5e559400c641f55d5dc38b6bc8bc302473f02445
Magnus Morton <magnusmorton@icloud.com>
2019-11-17 22:57:39 +0000

    added test. updated array

--- array.py
+++ array.py
@@ -1,12 +1,37 @@
 import numpy as np
 
+
+
+def cast(func):
+    def wrapper(*args, **kwargs):
+        arr = func(*args, **kwargs)
+        if not isinstance(arr,DelayArray):
+            arr = DelayArray(arr.shape, buffer=arr)
+        return arr
+    return wrapper
+
 class DelayArray:
     def __new__(cls, shape, dtype='float64', buffer=None, offset=0,
-                strides=None, order=None, ndarray=None):
+                strides=None, order=None):
         self = super(tparray, cls).__new__(cls)
-        if ndarray is not None:
-            self._ndarray = ndarray
+        if buffer is not None:
+            self._ndarray = buffer
         else:
             self._ndarray = np.ndarray(shape, dtype, buffer, offset, strides, order)
         self.shape = shape
         self.dtype = self._ndarray.dtype
+
+
+    def __repr__(self):
+        return "DELAYED"
+
+    def __array__(self):
+        return self._ndarray
+
+    def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
+        print(ufunc)
+        print(method)
+        return ufunc(*inputs, **kwargs)
+
+
+array = cast(np.array)
---
+++ test.py
@@ -0,0 +1,6 @@
+import array
+
+arr = array.array([1,2,3])
+
+
+arr * arr
