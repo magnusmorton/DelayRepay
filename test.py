473860d73112015c263bc1177289d6a1a9a2849a
Magnus Morton <magnus.morton@ed.ac.uk>
2020-01-27 16:17:57 +0000

    added proper testcases again

--- test.py
+++ test.py
@@ -1,51 +1,49 @@
 import unittest
 import logging
 import numpy as np
+import numpy.testing as npt
 from array import *
 
-# class TestScalarAdd(unittest.TestCase):
-#     def setUp(self):
-#         self.arr = array([1,2,3])
-#         self.res = self.arr + 1
-#         self.func = NumpyFunction(self.res.ex)
+class TestElWise(unittest.TestCase):
+    # def setUp(self):
+    #     self.arr = array([1,2,3])
+    #     self.res = self.arr + 1
+    #     self.func = NumpyFunction(self.res.ex)
 
-#     def test_e2e(self):
-#         print(self.func)
-#         exec(str(self.func), globals())
-#         print(jitfunc)
-#         print(self.func())
-#         print(self.res)
+    def test_scalar_add(self):
+        arr = ones(5000).astype(np.float32)
+        np_arr = np.ones(5000).astype(np.float32)
+        res = arr + 1
+        npt.assert_array_almost_equal(res, np_arr + 1)
 
+    def test_scalar_mul(self):
+        arr = ones(5000).astype(np.float32)
+        np_arr = np.ones(5000).astype(np.float32)
+        res = arr * 3
+        npt.assert_array_almost_equal(res, np_arr * 3)
 
-#     def test_numpy_function(self):
-#         print(self.res.ex)
-#         print(self.func)
+# arr = ones(5000000).astype(np.float32)
+# res = arr + 1
+# import cl
+# import num
 
-#     def test_gpu_function_def(self):
-#         print(self.func._gpu)
+# # trans = cl.GPUTransformer()
+# # res = trans.walk(res.ex)
+# # # print(res)
+# # # print(trans.ins)
+# # # print(trans.outs)
 
-arr = ones(5000000).astype(np.float32)
-res = arr + 1
-import cl
-import num
+# # args = cl.CLArgs(list(trans.ins.keys()) + trans.outs, ["float*", "float*"])
 
-# trans = cl.GPUTransformer()
-# res = trans.walk(res.ex)
-# # print(res)
-# # print(trans.ins)
-# # print(trans.outs)
+# # fun = cl.CLFunction(args, "gfunc", [res])
+# # # print(fun)
+# # emit = cl.CLEmitter()
+# # kern = emit.visit(fun)
+# # # print(kern)
 
-# args = cl.CLArgs(list(trans.ins.keys()) + trans.outs, ["float*", "float*"])
-
-# fun = cl.CLFunction(args, "gfunc", [res])
-# # print(fun)
-# emit = cl.CLEmitter()
-# kern = emit.visit(fun)
-# # print(kern)
-
-# print(cl.run_gpu(res.ex))
-print(res)
-# if __name__ == '__main__':
-#     # logging.basicConfig( level=logging.DEBUG)
-#     # unittest.main()
-#     pass
+# # print(cl.run_gpu(res.ex))
+# print(res)
+if __name__ == '__main__':
+    # logging.basicConfig( level=logging.DEBUG)
+    unittest.main()
+    
