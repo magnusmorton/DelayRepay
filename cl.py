9501c086fa1016be3c119466710648eb05a8c426
Magnus Morton <magnus.morton@ed.ac.uk>
2020-02-10 16:10:19 +0000

    PEP8ified

--- cl.py
+++ cl.py
@@ -9,18 +9,22 @@ import numpy as np
 
 logger = logging.getLogger("delayRepay.cl")
 
+
 @dataclass
 class CLTree:
     '''CL tree node'''
+
 
 @dataclass
 class Expression(CLTree):
     '''Expression'''
 
+
 @dataclass
 class DotExpression(Expression):
     arg1: Expression
     arg2: Expression
+
 
 @dataclass
 class BinaryExpression(Expression):
@@ -28,11 +32,13 @@ class BinaryExpression(Expression):
     left: Expression
     right: Expression
 
+
 @dataclass
 class Assignment(CLTree):
     '''Assignment statement'''
     left: Expression
     right: Expression
+
 
 @dataclass
 class Var(Expression):
@@ -45,20 +51,27 @@ class Var(Expression):
     def __eq__(self, other):
         return self.name is other.name
 
+
 @dataclass
 class Scalar(Expression):
     val: Number
-    
+
+    def to_input(self, _=0):
+        return self.val
+
+
 @dataclass
 class Subscript(Expression):
     '''a subscript e.g. a[i]'''
     var: Var
     sub: Expression
 
+
 @dataclass
 class CLArgs(CLTree):
     params: List[Var]
     types: List[str]
+
 
 @dataclass
 class CLFunction(CLTree):
@@ -68,13 +81,14 @@ class CLFunction(CLTree):
     body: List[CLTree]
 
 
-
 class CLEmitter(num.Visitor):
-    
+
     preamble = "int i = get_global_id(0);"
 
     def visit_BinaryExpression(self, node):
-        return "{} {} {}".format(self.visit(node.left), node.op, self.visit(node.right))
+        return "{} {} {}".format(
+            self.visit(node.left), node.op, self.visit(node.right)
+        )
 
     def visit_DotExpression(self, node):
         return "dot({}, {})"
@@ -92,11 +106,17 @@ class CLEmitter(num.Visitor):
         return "{} = {};".format(self.visit(node.left), self.visit(node.right))
 
     def visit_CLArgs(self, node):
-        args = ["__global {} {}".format(typ, var) for typ, var in zip(node.types, self.visit(node.params)) ]
+        args = ["__global {} {}".format(typ, var)
+                for typ, var in zip(node.types, self.visit(node.params))]
         return ", ".join(args)
 
     def visit_CLFunction(self, node):
-        return "__kernel void {} ({}) {{\n{}\n{}\n}}".format(node.name, self.visit(node.args), self.preamble,  "\n".join(self.visit(node.body)))
+        return "__kernel void {} ({}) {{\n{}\n{}\n}}".format(
+            node.name, self.visit(node.args),
+            self.preamble,
+            "\n".join(self.visit(node.body))
+        )
+
 
 class CLVarVisitor(num.Visitor):
     pass
@@ -111,7 +131,8 @@ class GPUTransformer(num.NumpyVisitor):
 
     def visit_BinaryNumpyEx(self, node):
         cur_visits = self.visits
-        ex = BinaryExpression(node.to_op(), self.visit(node.left), self.visit(node.right))
+        ex = BinaryExpression(node.to_op(), self.visit(node.left),
+                              self.visit(node.right))
         if cur_visits == 1:
             ex = Assignment(Subscript(Var("foo"), Var("i")), ex)
             self.outs.append(Var("foo"))
@@ -129,25 +150,33 @@ class GPUTransformer(num.NumpyVisitor):
         ex = DotExpression(self.visit(node.arg1), self.visit(node.arg2))
         return ex
 
+
 def function_coverter(tree: CLTree):
     pass
+
 
 def executor(kernel, in_arrs, out_arr):
     ctx = cl.create_some_context()
     queue = cl.CommandQueue(ctx)
     mf = cl.mem_flags
-    in_bufs = [cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=in_arr) for in_arr in in_arrs]
-    prog = cl.Program(ctx,kernel).build()
+    in_bufs = [cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=in_arr)
+               for in_arr in in_arrs]
+    prog = cl.Program(ctx, kernel).build()
     res_g = cl.Buffer(ctx, mf.WRITE_ONLY, in_arrs[0].nbytes)
     prog.gfunc(queue, in_arrs[0].shape, None, *in_bufs, res_g)
     res_np = np.empty_like(in_arrs[0])
     cl.enqueue_copy(queue, res_np, res_g)
     return res_np
 
+
 def run_gpu(numpy_ex):
-    transformer =  GPUTransformer()
+    transformer = GPUTransformer()
     gpu_ex = transformer.walk(numpy_ex)
-    args = CLArgs(list(transformer.ins.keys()) + transformer.outs, ["float*"] * (len(transformer.ins) + len(transformer.outs)))
+    import pdb
+    pdb.set_trace()
+    # This is probably one of the worst lines of code I've ever written
+    args = CLArgs(list(transformer.ins.keys()) + transformer.outs,
+                  ["float*"] * (len(transformer.ins) + len(transformer.outs)))
     func = CLFunction(args, "gfunc", [gpu_ex])
     kernel = CLEmitter().visit(func)
     return executor(kernel, list(transformer.ins.values()), None)
