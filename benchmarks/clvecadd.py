162b3f5cf0dbddd5c40ef9d3717a37ae0008f907
Magnus Morton <magnus.morton@ed.ac.uk>
2020-02-26 17:12:15 +0000

    enabled clvecadd again

--- benchmarks/clvecadd.py
+++ benchmarks/clvecadd.py
@@ -1,10 +1,7 @@
-# import importlib
-# import sys
-# import numpy as nup
+import sys
+import numpy as nup
+import delayarray as np
+SIZE = 1024000
+mat = np.full((SIZE,), 7).astype(nup.float32)
 
-# module = sys.argv[1]
-# np = importlib.import_module(module)
-# SIZE = 1024000
-# mat = np.full((SIZE,), 7).astype(nup.float32)
-
-# print((mat * 3 + 9).dot( mat))
+print((mat * 3 + 9).dot(mat))
