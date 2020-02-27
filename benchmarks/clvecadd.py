93c5d787c42be6fc1fe8b7537c84f14df4a3a7af
Magnus Morton <magnus.morton@ed.ac.uk>
2020-02-27 11:44:21 +0000

    deleted difficult benchmarks

--- benchmarks/clvecadd.py
+++
@@ -1,7 +0,0 @@
-import numpy as nup
-import delayarray as np
-
-SIZE = 1024000
-mat = np.full((SIZE,), 7).astype(nup.float32)
-
-print((mat * 3 + 9).dot(mat))
--- benchmarks/npmul.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-SIZE = 10240
-mat = np.full((SIZE,SIZE), 7).astype(np.float32)
-
-print(mat* 3)
