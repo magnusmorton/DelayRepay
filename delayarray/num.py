5cc129640f5fed04cc5ccc4e0f204af2a721167f
Magnus Morton <magnus.morton@ed.ac.uk>
2020-03-04 13:51:04 +0000

    removed dead code

--- delayarray/num.py
+++ delayarray/num.py
@@ -89,26 +89,6 @@ class DotEx(NumpyEx, Funcable):
         self.shape = calc_shape(left.shape, right.shape, np.dot)
         self._inshape  = left.shape
 
-# @dataclass
-# class Dot(BinaryNumpyEx):
-#     '''np.dot func'''
-#     func: np.ufunc = np.dot
-
-# @dataclass
-# class Multiply(BinaryNumpyEx):
-#     '''np.multiply'''
-#     func = np.multiply
-
-# @dataclass
-# class Matmul(BinaryNumpyEx):
-#     '''np.matmul'''
-#     func = np.matmul
-
-# @dataclass
-# class Add(BinaryNumpyEx):
-#     '''np.add'''
-#     func  = np.add
-
 
 class NPArray(NumpyEx):
     '''ndarray'''
@@ -123,13 +103,6 @@ class NPArray(NumpyEx):
     def __eq__(self, other):
         return self.array is other.array
 
-# @dataclass
-# class Var(NPAtom):
-    # '''a variable'''
-    # name: str
-
-    # def __repr__(self):
-        # return self.name
 
 class Scalar(NumpyEx):
     '''a scalar'''
@@ -196,71 +169,7 @@ class NumpyVisitor(Visitor):
         return self.visit(tree)
 
 
-# class NumpyVarVisitor(NumpyVisitor):
-    # '''visits and returns new tree with arrays replaced with vars'''
-    # def __init__(self):
-        # self.arrays = {}
-        # super(NumpyVarVisitor, self).__init__()
 
-    # def visit_BinaryNumpyEx(self, node):
-        # '''visit BinaryNumpyEx'''
-        # return type(node)(
-            # self.visit(node.left),
-            # self.visit(node.right)
-        # )
-
-    # def visit_NPArray(self, node):
-        # '''visit NPArray'''
-        # if node in self.arrays:
-            # name = self.arrays[node]
-        # else:
-            # name = "npvar" + str(len(self.arrays))
-            # self.arrays[node] = name
-        # return Var(name)
-
-    # def visit_Scalar(self, node):
-        # '''Visit scalar'''
-        # return node
-
-# class StringVisitor(NumpyVisitor):
-    # '''returns string of numpy expression'''
-
-    # def visit_binary(self, node: BinaryNumpyEx):
-        # '''visit BinaryNumpyEx'''
-        # return "{}({}, {})".format("np." + node.func.__name__, self.visit(node.left), self.visit(node.right))
-
-    # def visit_var(self, node: Var):
-        # return node.name
-
-    # def visit_scalar(self, node: Scalar):
-        # return node.val
-
-# class NumpyFunction:
-    # '''complete numpy function'''
-   
-    # def __init__(self, body: NumpyEx):
-        # visitor = NumpyVarVisitor()
-        # transformed = visitor.visit(body)
-        # self.body = transformed
-        # self.args = visitor.arrays
-
-    # def __call__(self):
-        # visitor = StringVisitor()
-        # exp = str(self)
-        # args = {}
-        # exec(exp, globals(), args )
-        # return args['jitfunc'](*[arr.array for arr in self.args.keys()])
-
-    # def _cpu(self):
-        # string = StringVisitor().visit(self.body)
-        # return "import numba\n@numba.jit\ndef jitfunc({}):\n    {}".format(','.join(self.args.values()), string)
-
-    # def _gpu(self):
-        
-        # raise Error("GPU stuff not complete")
-
-    # def __str__(self):
-        # return self._cpu()
 def is_matrix_matrix(left, right):
     return len(left) > 1 and len(right) > 1
 
@@ -309,47 +218,14 @@ class ShapeAnnotator(NumpyVisitor):
         node._inshape = left.shape
         return node
 
-    # def visit_MMEx(self, node):
-    #     left = self.visit(node.arg1)
-    #     right = self.visit(node.arg2)
-    #     node.shape = ShapeAnnotator.calc_shape(left.shape, right.shape, np.dot)
-    #     node._inshape = left.shape
-    #     return node
 
-    # def visit_MVEx(self, node):
-    #     left = self.visit(node.arg1)
-    #     right = self.visit(node.arg2)
-    #     node.shape = ShapeAnnotator.calc_shape(left.shape, right.shape, np.dot)
-    #     node._inshape = left.shape
-    #     return node
     
 class ReduceTransformer(NumpyVisitor):
     def visit_DotEx(self, node):
         # TODO This is just for vector x vector
         left = self.visit(node.arg1)
         right = self.visit(node.arg2)
-        # if (len(left.array.shape) > 1 and len(right.array.shape) > 1):
-        #     if (left.array.shape[0] > 1 and left.array.shape[1] > 1) and (right.array.shape[0] > 1 and right.array.shape[1] > 1):
-        #         # print("matrix x matrix")
-        #         muls = DotEx(left, right)
-        #         muls.shape = (left.array.shape[0],right.array.shape[0])
-        #         return muls
-        #     elif is_matrix_vector(left.shape, right.shape):
-        #         # matrix x vector
-        #         print("matrix x vector")
-        #         return MVEx(left, right, node.shape, node._inshape)
-                
-        #     elif (left.array.shape[0] > 1 or left.array.shape[1] > 1) and (right.array.shape[0] > 1 or right.array.shape[1] > 1):
-        #         # vector x vector
-        #         print("vector x vector")
-        #         muls = BinaryNumpyEx(left, right, np.multiply)
-        #         muls.shape = node._inshape
-        #         red = ReduceEx(np.add, muls)
-        #         red.shape = node.shape
-        #         red._inshape = node._inshape
-        #         return red
-        # else:
-        #     print("scalar?")
+        
         if is_matrix_vector(left.shape, right.shape):
             print("matrix x vector")
             return MVEx(left, right, node.shape, node._inshape)
