810e65c7577cadf1c50198d3c34ae7d04145c028
Magnus Morton <magnus.morton@ed.ac.uk>
2020-04-02 19:11:58 +0100

    testing cupy fuse

--- tests/bench.py
+++ tests/bench.py
@@ -1,3 +1,8 @@
-import delayarray as m
-data = m.random.random((1000, 1000))
-print(m.sin(data) ** 2 + m.cos(data) ** 2)
+import cupy
+data = cupy.random.random((1000, 1000))
+
+#@cupy.fuse(kernel_name='func')
+def func(arg):
+    return cupy.sin(arg) ** 2 + cupy.cos(arg) ** 2
+
+print(func(data))
