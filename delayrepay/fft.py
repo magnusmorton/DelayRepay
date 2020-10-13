ee027801ffd2b1d041f20d1f437203c8e70aa55f
Magnus Morton <m@magnusmorton.com>
2020-10-13 18:01:35 +0100

    GPL headers

--- delayrepay/fft.py
+++ delayrepay/fft.py
@@ -1,3 +1,20 @@
+""" numpy.fft wrapper
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+# Copyright (C) 2020 by University of Edinburgh
+
+
 import cupy.fft
 from .cuarray import DelayArray
 
@@ -8,4 +25,3 @@ def fft(self, *args, **kwargs):
             for arg in args]
     print(nargs)
     return cupy.fft.fft(*nargs, **kwargs)
-    
