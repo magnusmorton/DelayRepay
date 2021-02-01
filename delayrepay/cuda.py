a258f4256ffeb1a92a2e03c1784f0aed8d1a8491
Magnus Morton <magnus@morton.ai>
2021-02-01 17:37:02 +0000

    cleaned up cuda.py

--- delayrepay/cuda.py
+++ delayrepay/cuda.py
@@ -14,7 +14,6 @@ this program. If not, see <http://www.gnu.org/licenses/>.
 """
 # Copyright (C) 2020 by Univeristy of Edinburgh
 
-from typing import List, Dict, Tuple
 import cupy  # type: ignore
 
 from .visitor import Visitor
@@ -22,29 +21,9 @@ from .visitor import Visitor
 np = cupy
 fallback = cupy
 
-Shape = Tuple[int, int]
-
-InputDict = Dict[str, "BaseFragment"]
-
 
 def is_ndarray(arr):
     return isinstance(arr, cupy.core.core.ndarray)
-
-
-class BaseFragment:
-    def __init__(self):
-        self.name = None
-        self.stmts = []
-        self._expr = None
-        self._inputs = {}
-
-    @property
-    def inputs(self) -> InputDict:
-        return self._inputs
-
-    @property
-    def kernel_args(self) -> InputDict:
-        return self._inputs
 
 
 def dedup(seq):
@@ -53,37 +32,8 @@ def dedup(seq):
     return [x for x in seq if not (x in seen or seen_add(x))]
 
 
-class Fragment(BaseFragment):
-    def __init__(self, name: str, stmts: List[str], inputs: InputDict) -> None:
-        self.name = name
-        self.stmts = stmts
-        self._inputs = inputs
-        # self.dtype = np.float32
-
-    def ref(self) -> str:
-        return self.name
-
-    # def expr(self) -> str:
-    #     return self._expr
-
-    def to_input(self):
-        return {self.name: self.node.array}
-
-    def to_kern(self) -> cupy.ElementwiseKernel:
-        body = ";\n".join(dedup(self.stmts))
-        inargs = [f"T {arg}" for arg in self.kernel_args]
-        kern = cupy.ElementwiseKernel(
-            ",".join(inargs),
-            "T out",
-            f"{body};\nout = {self.name}",
-            f"delay_repay_{self.name}",
-        )
-        return kern
-
-
 def make_kernel(inputs, body, name):
     body = ";\n".join(dedup(body))
-    print(body)
     inargs = [f"T {arg}" for arg in inputs]
     return cupy.ElementwiseKernel(
         ",".join(inargs),
@@ -93,113 +43,7 @@ def make_kernel(inputs, body, name):
     )
 
 
-class InputFragment(BaseFragment):
-    def __init__(self, name, arr) -> None:
-        super().__init__()
-        self.name = name
-        self._inputs = {self.name: arr}
-
-    def ref(self) -> str:
-        return f"{self.name}"
-
-    def expr(self) -> str:
-        return f"{self.name}"
-
-
-class ScalarFragment(BaseFragment):
-    def __init__(self, val) -> None:
-        super().__init__()
-        self.val = val.val
-        self.dtype = val.dtype
-
-    def ref(self) -> str:
-        return str(self.val)
-
-    def expr(self) -> str:
-        return str(self.val)
-
-
-class ReductionKernel(Fragment):
-    def to_kern(self):
-        kern = cupy.ReductionKernel(
-            ",".join(self.inargs),
-            "T out",
-            self.expr,
-            self.redex,
-            "out = a",
-            0,
-            self.name,
-        )
-        return kern
-
-
-def combine_inputs(*args: InputDict) -> InputDict:
-    ret = {}
-    for arg in args:
-        ret.update(arg)
-    return ret
-
-
 class CupyEmitter(Visitor):
-    def __init__(self):
-        super().__init__()
-        self.ins = {}
-        self.outs = []
-        self.kernels = []
-        self.seen = {}
-        self.count = 0
-
-    def visit(self, node):
-        if node in self.seen:
-            visited = self.seen[node]
-        else:
-            visited = super().visit(node)
-            self.seen[node] = visited
-            self.count += 1
-        return visited
-
-    def visit_BinaryNumpyEx(self, node) -> BaseFragment:
-        op = node.to_op()
-        left = self.visit(node.children[0])
-        right = self.visit(node.children[1])
-        name = f"binex{self.count}"
-        stmt = f"T {name} = {left.ref()} {op} {right.ref()}"
-        stmts = left.stmts + right.stmts + [stmt]
-        return Fragment(name, stmts, combine_inputs(left.inputs, right.inputs))
-
-    def visit_UnaryFuncEx(self, node) -> BaseFragment:
-        inner = self.visit(node.children[0])
-        name = f"unfunc{self.count}"
-        stmts = inner.stmts + [f"T {name} = {node.to_op()}({inner.ref()})"]
-        return Fragment(name, stmts, inner.inputs)
-
-    def visit_BinaryFuncEx(self, node) -> BaseFragment:
-        op = node.to_op()
-        left = self.visit(node.children[0])
-        right = self.visit(node.children[1])
-        name = f"binfunc{self.count}"
-        stmt = f"T {name} = {op}({left.ref()}, {right.ref()})"
-        stmts = left.stmts + right.stmts + [stmt]
-        return Fragment(name, stmts, combine_inputs(left.inputs, right.inputs))
-
-    def visit_NPArray(self, node) -> BaseFragment:
-        return InputFragment(f"arr{self.count}", node)
-
-    def visit_NPRef(self, node) -> BaseFragment:
-        return InputFragment(f"ref{self.count}", node)
-
-    def visit_Scalar(self, node) -> BaseFragment:
-        return ScalarFragment(node)
-
-    def visit_ReduceEx(self, node) -> BaseFragment:
-        inner = self.visit(node.children[0])
-        name = node.name
-        op = node.to_op()
-
-        return NotImplemented
-
-
-class NuCupyEmitter(Visitor):
     def __init__(self):
         super().__init__()
         self.ins = {}
@@ -254,19 +98,8 @@ class NuCupyEmitter(Visitor):
         return NotImplemented
 
 
-def oldrun(ex) -> cupy.array:
-    visitor = CupyEmitter()
-    kerns = [visitor.visit(ex)]
-    for kern in kerns:
-        compiled = kern.to_kern()
-        inputs = [value.array for key, value in kern.kernel_args.items()]
-        ret = compiled(*inputs)
-        kern.array = ret
-    return ret
-
-
 def run(ex) -> cupy.array:
-    visitor = NuCupyEmitter()
+    visitor = CupyEmitter()
     body = visitor.visit(ex)[1]
     kern = make_kernel(ex.inputs, body, ex.name)
     inputs = [value.array for value in ex.inputs.values()]
