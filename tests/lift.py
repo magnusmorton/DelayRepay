8aeb88b342e742b86d493a73bba79ea3d6ecb493
Magnus Morton <magnus@morton.ai>
2021-02-12 18:48:20 +0000

    added two array test

--- tests/lift.py
+++ tests/lift.py
@@ -19,6 +19,11 @@ class TestLift(unittest.TestCase):
         arr = dr.ones(32)
         print(arr*3 + 2)
 
+    def test_two_arrs(self):
+        arr1 = dr.ones(32)
+        arr2 = dr.full((32,),3)
+        print(arr1 + arr2)
+
 
 if __name__ == '__main__':
     unittest.main()
